ea36ede4ddd858f95b23973629622fd1a5f9ac2e
erik <erikfirdaus03@gmail.com>
2024-12-19 08:20:39 +0700

    test

--- index.blade.php
+++ index.blade.php
@@ -10,15 +10,5 @@
     <title>Anak Baik TI</title>
 </head>
 <body>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam corrupti voluptate neque corporis incidunt cumque nihil officia enim sequi? Nesciunt ad illum eos dolore unde, vero sed quis enim deserunt!
-    Ut explicabo a aperiam, illo impedit assumenda exercitationem enim! Incidunt, obcaecati numquam, rem quibusdam aut, quo illo et natus culpa fugit dolor porro fugiat excepturi veritatis similique deserunt cum amet?
-    Architecto nam vel, aliquam deserunt iste placeat voluptates eum aut harum ea a officia quo animi veniam quia laboriosam similique, aliquid, tempora sed nihil dignissimos natus. Non ipsum saepe illum.
-    Repellat magnam cumque reprehenderit vero molestiae. Nisi voluptate amet dolorem ad nostrum? Hic optio unde cupiditate vitae tenetur ipsam in esse, animi, saepe accusantium ratione velit quis repellat aut ipsa.
-    Amet, beatae cum at dignissimos explicabo pariatur delectus nulla sapiente aspernatur veritatis iste officia quisquam, ut rem eligendi? Ut soluta impedit repudiandae a voluptatibus inventore officia, ducimus qui aspernatur at?
-    Quas at veniam et repellat harum libero, molestias minima praesentium culpa consequuntur ad voluptates vel mollitia. Rem, sit ad? Repellendus illum laudantium inventore aliquid quod quas nam, suscipit sunt esse!
-    Quaerat veniam totam non recusandae numquam quidem quisquam accusantium provident pariatur tempore tempora porro, quae ab nostrum, impedit fuga iste nemo ex eveniet eligendi repellat temporibus beatae dolorum. Ratione, soluta.
-    Aliquam velit ea quisquam iure veritatis commodi. Ex, laborum numquam illo laudantium a nostrum eius magnam, sequi delectus, labore repellendus inventore quasi omnis quia! Sed eveniet quae beatae animi ullam!
-    Ab labore incidunt, repellat rem maiores inventore fuga iure culpa velit impedit assumenda nemo iusto corrupti error quaerat odit nesciunt soluta doloribus, minus blanditiis ratione alias. Dolorem placeat iste architecto?
-    Odio sunt, maiores illum magni qui molestiae ab, itaque modi saepe repellat ducimus doloribus labore quasi quis minima deleniti in similique delectus et quas. Iure ab hic repellat odio quasi?</p>
-</body>
+    <p>test</p></body>
 </html>
